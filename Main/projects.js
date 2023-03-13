f5a4e7a338c32999cedd94ca4b926047c7765183
Peter <petershotbox@gmail.com>
2023-03-13 01:41:01 +0000

    Added infinate project scroll

--- Main/projects.js
+++ Main/projects.js
@@ -1,24 +1,32 @@
 const projects = document.querySelectorAll(".project").length;
 let scrollDist;
 let scrollMax;
-
+let clickCount = -1;
 checkSize();
 
 function checkSize(){
-    scrollDist =((window.innerWidth > 1200)? 1000:500);
+    scrollDist =((window.innerWidth > 1200)? 1000:450);
     scrollMax = projects * scrollDist;
+    clickCount ++;
 }
 
 document.querySelectorAll(".arrow")[0].addEventListener("click", leftClick);
 document.querySelectorAll(".arrow")[1].addEventListener("click", rightClick);
 
+document.querySelector('.projectsContainer').scrollLeft = scrollDist;
+
 function leftClick(){
     checkSize();
     let pos = document.querySelector('.projectsContainer').scrollLeft;
     if(pos-scrollDist > 0){
+        document.querySelector('.projectsContainer').style.scrollBehavior = "smooth";
         document.querySelector('.projectsContainer').scrollLeft = pos - scrollDist;
     }else{
-        document.querySelector('.projectsContainer').scrollLeft = scrollMax;
+        //document.querySelector('.projectsContainer').scrollLeft = scrollMax;
+        document.querySelector('.projectsContainer').style.scrollBehavior = "auto";
+        document.querySelector('.projectsContainer').scrollTo(scrollMax-scrollDist, 0);
+        document.querySelector('.projectsContainer').style.scrollBehavior = "smooth";
+        document.querySelector('.projectsContainer').scrollLeft = scrollMax - scrollDist - scrollDist;
     }
 }
 
@@ -26,11 +34,29 @@ function rightClick(){
     checkSize();
     let pos = document.querySelector('.projectsContainer').scrollLeft;
     if(pos+scrollDist < scrollMax){
+        document.querySelector('.projectsContainer').style.scrollBehavior = "smooth";
         document.querySelector('.projectsContainer').scrollLeft = pos + scrollDist;
     }else{
-        document.querySelector('.projectsContainer').scrollLeft = 0;
+        //document.querySelector('.projectsContainer').scrollLeft = 0;
+        document.querySelector('.projectsContainer').style.scrollBehavior = "auto";
+        document.querySelector('.projectsContainer').scrollTo(scrollDist, 0);
+        document.querySelector('.projectsContainer').style.scrollBehavior = "smooth";
+        pos = document.querySelector('.projectsContainer').scrollLeft;
+        document.querySelector('.projectsContainer').scrollLeft = pos + scrollDist;
     }
 }
 
 //document.querySelector('.projectsContainer').addEventListener("scroll", function(e){console.log(e.target.scrollLeft)})
-
+let scrollsPassed = 0;
+setInterval(()=>{
+    console.log(clickCount)
+    if(clickCount == 0){
+        clickCount -= 1;
+        rightClick();
+    }else{
+        scrollsPassed ++;
+        if(scrollsPassed > 4){
+            clickCount = 0;
+        }
+    }
+}, 8000)
